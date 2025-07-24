6f0a89e8507996ba24023f6c72f98364e46163ab
Derek Sagun <dereksagun@Dereks-MacBook-Pro.local>
2025-07-23 21:24:15 -0700

    Refactored Code to separate the controller and mongoose model into different modules

--- index.js
+++ index.js
@@ -1,36 +1,16 @@
 require('dotenv').config()
 const express = require('express')
+const Blog = require('./models/blog')
+const blogRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
 
 const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+app.use('/api/blogs', blogRouter)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
